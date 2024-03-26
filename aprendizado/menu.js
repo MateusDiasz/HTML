b4123b6b39e85192ecdc1f2560faedfe52e8e6df
MateusDiasz <mateus.dsilva8@senacsp.edu.br>
2024-03-26 17:32:14 -0300

    matriz

---
+++ aprendizado/menu.js
@@ -0,0 +1,11 @@
+//function menu(){
+// let menu = document.getElementById("menu");
+// if(menu.style.display =="block"){
+// menu.style.display = "none"
+//}else{
+//menu.style.display = "block"
+//}
+//}
+let icon = document.getElementsByClassName("icon");
+let menu = document.getElementById("menu");
+
