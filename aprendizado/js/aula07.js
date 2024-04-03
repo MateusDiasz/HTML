dc1cd6ead0bd8847b780ba05e34df5022c284f08
MateusDiasz <mateus.dsilva8@senacsp.edu.br>
2024-04-03 17:24:30 -0300

    commit AULA 03/04/2024

--- aprendizado/js/aula07.js
+++ aprendizado/js/aula07.js
@@ -1,5 +1,5 @@
 let vetor = []
-for (let posicao = 0; posicao<vetor.length; posicao++){
+for (let posicao = 0; posicao < vetor.length; posicao++) {
     console.log(vetor[posicao])
 }
 let formContato = document.getElementById('form-contato')
@@ -11,13 +11,41 @@ let assunto = []
 let msg = []
 
 //Inserindo dados nos meus arrays
-formContato.addEventListener("submit", (event)=>{
+formContato.addEventListener("submit", (event) => {
     event.preventDefault();
     nome.push(formContato.nome.value);
     email.push(formContato.email.value);
     telefone.push(formContato.telefone.value);
     assunto.push(formContato.mensagem.value);
-    gerarLista
-
-
+    gerarLista()
 })
+function gerarLista() {
+
+    let auxiliarLista = "";
+
+    for (let posicao = 0; posicao < nome.length; posicao++) {
+        
+        auxiliarLista += `<div class="card">       
+                        <div class="dados-label">
+                          <div> Nome completo:</div>
+                          <div>${nome[posicao]}</div>
+                        </div>
+                        <div class="dados-label">
+                          <div> Telefone:</div>
+                          <div>${telefone[posicao]}</div>
+                         </div>
+                        <div class="dados-label">
+                          <div> Email:</div>
+                          <div>${email[posicao]}</div>
+                        </div>
+                        <div class="dados-label">
+                          <div> assunto:</div>
+                          <div>${assunto[posicao]}</div>
+                        </div>
+                        <div class="dados-label">
+                          <div> Mesagem:</div>
+                          <div>${msg[posicao]}</div>
+                        </div> ` 
+    }
+    divLista.innerHTML = auxiliarLista;
+}
