923bedd71e3c85d04022485d7638a7eefbcdca50
MateusDiasz <mateus.dsilva8@senacsp.edu.br>
2024-04-04 17:23:07 -0300

    mova-se jquery

---
+++ projeto_MovaSe/js/interacoes.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#destaque').slick({
+        autoplay:true,
+        autoplaySpeed: 4000,
+        dots:true,
+        arrows:false,
+    })
+})
+
+/*crio minha variael menu;
+faço minha variavel receber o id de onde eu quero que seja o botão;
+tenho que fazer meu menu receber home, pacotes, acessorios e contato;
+Tenho que esconher o home, pacotes, acessorios e contato,
+usar uma função para pegar meu botão que é minha variavel menu
+ao pertar meu botão ela aparece em forma d menu
+ao apertar novamente ela some e volta a pagina inicial*/
+
+
+
+
